b9dcf7dfc011037a7ac62082d787e90fe5e02fa2
mrtlckn <ahmet.telceken@hotmail.com>
2022-03-28 11:15:31 +0300

    user func

---
+++ User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp1
+{
+    class User
+    {
+        public string[] Username = { "admin","user"};
+        public string[] Password = { "admin", "user" };
+
+
+       
+
+        
+        public bool Control(string name,string pass)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                if (Username[i] == name && Password[i] == pass)
+                    return true;
+
+            }
+            return false;
+        }
+
+    }
+}
